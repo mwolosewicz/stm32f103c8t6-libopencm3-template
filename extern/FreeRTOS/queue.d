72daa73cf18f0b6982f4cdba5dfed3676e998ca4
Maciej Wołosewicz <maciej.wolosewicz@gmail.com>
2021-02-23 21:06:08 +0100

    Add FreeRTOS files.

---
+++ extern/FreeRTOS/queue.d
@@ -0,0 +1,17 @@
+extern/FreeRTOS/queue.o: extern/FreeRTOS/queue.c \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/stdlib.h /usr/include/newlib/string.h \
+ /usr/include/newlib/sys/string.h extern/FreeRTOS/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h src/FreeRTOSConfig.h \
+ extern/FreeRTOS/projdefs.h extern/FreeRTOS/portable.h \
+ extern/FreeRTOS/deprecated_definitions.h extern/FreeRTOS/portmacro.h \
+ extern/FreeRTOS/mpu_wrappers.h extern/FreeRTOS/task.h \
+ extern/FreeRTOS/list.h extern/FreeRTOS/queue.h
